c88df39253560f4b5aba2daae7c9b0e3133fcbbf
Zuikaku28 <47487381+Zuikaku28@users.noreply.github.com>
2019-02-10 05:56:50 +0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,25 @@
+const Discord = require('discord.js');
+
+const client = new Discord.Client();
+
+client.on('ready', () => {
+    console.log('I am ready!');
+  });
+  client.on('message', message => {
+    if (message.content === 'zuikaku') {
+        message.channel.send('Apa Sayang ??^_^');
+    }
+
+    if (message.content === 'kamu siapa?') {
+        message.channel.send('aku...dan han chan sama karena han chan membuat ku seperti ini^_^') 
+    }
+        
+    if (message.content === 'dito kamu kenal?') {
+        message.channel.send('aku kenal dia teitoku ku aku dulu kenal omelnya fufufu.. tapi dito ku suka dia baik banget biar pun aku di omelin saat pertemuan pertama :( tapii... kini dito suka sama tita aku sudah tidak di perdulikan lagiT_T')
+    }
+    
+    if (message.content === ':(') {
+        message.channel.send('aku cuman mau dito menepati janjinya saja menikah dengan ku T_T')
+  }  
+});
+client.login('NTQzODczMjEwOTM3MTE0NjI1.D0C8Gg.1cKNC_ZEF8mCOl5PKmqsVAa3AWo');
